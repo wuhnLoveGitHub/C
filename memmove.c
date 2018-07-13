82dbadbcd5ef880b9f23276a7fa790efb8eaddeb
wuhn <wuhn@cssweb.com.cn>
2018-07-12 23:49:23 -0700

    add memmove

---
+++ memmove.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char *argv[])
+{
+    char str[100] = "abcdefg";
+    //åˆ éa
+    strcpy(str,str+1);
+    puts(str);
+    char buf[100] = "abcdefg";
+    //a->xyz
+    memmove(buf+1,buf+1,2);
+    puts(buf);
+}
