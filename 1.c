71383d410ea9bc1a464380c52d02765216e5a808
wuhn <wuhn@cssweb.com.cn>
2018-07-13 00:36:11 -0700

    test

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//aaa
+int main()
+{
+    int i = 0;
+    char arr[10] = {0};
+    for(i = 0;i < 5;i++)
+    {
+        arr[i] = i;
+        printf("i = %d arr[i] = %d\n",i,arr[i]);
+    }
+    printf("i = %d\n",i);
+}
